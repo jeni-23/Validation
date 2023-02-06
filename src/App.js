4b81900139e8572aac6512ff8d7de73c4e7a5b5f
JeniAro <88921564+JeniAro@users.noreply.github.com>
2023-02-06 13:55:35 +0530

    form

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Validation from './Components/validation';
+import Home from './Components/home'
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+     <Routes>
+        <Route path="/" element={ <Validation/>} />
+        <Route path="/home" element={ <Home/>} />
+       
+      </Routes>
+  
+  );
+}
+
+export default App;
---
+++ src/Components/home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Home=()=>{
+	return(
+<div><h1 style={{color:'white',textAlign:'center',fontWeight:1000}}>Welcome Our website</h1></div>
+		)
+}
+export default Home;
