48acf0755ecff258bd37b19b660c44bcfa838bee
JeniAro <88921564+JeniAro@users.noreply.github.com>
2023-03-07 16:38:16 +0530

    'changes'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Validation from './Components/validation';
 import Home from './Components/home'
@@ -6,11 +6,11 @@ import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-     <Routes>
-        <Route path="/" element={ <Validation/>} />
-        <Route path="/home" element={ <Home/>} />
-       
-      </Routes>
+        <Routes>
+           <Route path="/" element={ <Validation/>} />
+           <Route path="/home" element={ <Home/>} />
+          
+         </Routes>
   
   );
 }
